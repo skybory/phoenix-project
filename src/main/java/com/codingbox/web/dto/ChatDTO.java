fa345754940e081fbe59964929b455b4179246c9
조수영 <tbdud98@gmail.com>
2024-03-08 12:11:11 +0900

    'makeDTO'

--- src/main/java/com/codingbox/web/dto/ChatDTO.java
+++ src/main/java/com/codingbox/web/dto/ChatDTO.java
@@ -5,25 +5,39 @@ public class ChatDTO {
 	
 	// 변수 설정
 	
-	private String sample1;
-	private int sample2;
-	
+    private String fromId;
+    private String toId;
+    private String contents;
+    private String chatTime;
+	public String getFromId() {
+		return fromId;
+	}
+	public void setFromId(String fromId) {
+		this.fromId = fromId;
+	}
+	public String getToId() {
+		return toId;
+	}
+	public void setToId(String toId) {
+		this.toId = toId;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getChatTime() {
+		return chatTime;
+	}
+	public void setChatTime(String chatTime) {
+		this.chatTime = chatTime;
+	}
 	
 	
 	// 게터 세터 추가
 	
-	public String getSample1() {
-		return sample1;
-	}
-	public void setSample1(String sample1) {
-		this.sample1 = sample1;
-	}
-	public int getB() {
-		return sample2;
-	}
-	public void setB(int b) {
-		this.sample2 = b;
-	}
+
 	
 	
 	
