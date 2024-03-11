531e5e07e605ae262733769bd7f06f8969b945dd
kjg <kgggo759@gmail.com>
2024-03-11 15:58:48 +0900

    MyPageFrontController 작성 240311

---
+++ src/main/java/com/lemonmarket/web/servlet/MyPageFrontController.java
@@ -0,0 +1,67 @@
+package com.lemonmarket.web.servlet;
+
+import java.io.IOException;
+
+import com.lemonmarket.web.action.ActionForward;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("*.mypage")
+public class MyPageFrontController extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+	
+	protected void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException{
+		String requestURL = req.getRequestURI();
+		ActionForward forward = null;
+		
+		switch(requestURL) {
+		
+		case "/board/Location.mypage":
+			System.out.println("마이페이지 버튼 클릭");
+			forward = new ActionForward(true, "/board/MyPage.jsp");
+			break;
+		
+		case "/mypage/InterestList.mypage":
+			System.out.println("관심목록 버튼클릭");
+			forward = new MyInterestListAction().execute(req,resp);
+			break;
+			
+		case "/mypage/SalesDetails.mypage":
+			System.out.println("판매내역 버튼클릭");
+			forward = new MySalesDetailsAction().execute(req,resp);
+			break;
+			
+		case "/mypage/PurchaseDetails.mypage":
+			System.out.println("구매내역 버튼클릭");
+			forward = new MyPurchaseDetailsAction().execute(req,resp);
+			break;
+		
+		case "/mypage/ProfileView" :
+			System.out.println("개인정보 보기 버튼클릭");
+			forward = new MyProfileViewAction().execute(req,resp);
+			break;
+			
+		}
+		
+		
+		if(forward != null) {
+			if(forward.isRedirect()) {
+				resp.sendRedirect(forward.getPath());
+			} else {
+				req.getRequestDispatcher(forward.getPath()).forward(req, resp);
+			}
+		}
+	}
+}
